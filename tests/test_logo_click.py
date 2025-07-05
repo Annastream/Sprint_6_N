e9957d4b7e7ff217be66ae5e07b55de1856116bb
Anna Antipova <anttianna@yandex.ru>
2025-07-05 16:25:44 +0300

    Commit_2

--- tests/test_logo_click.py
+++ tests/test_logo_click.py
@@ -2,6 +2,7 @@ import pytest
 import allure
 from pages.main_page import MainPage
 
+
 class TestLogo:
     @allure.title('Проверка перехода на главную страницу сервиса при клике на лого "Самокат" в шапке.')
     def test_logo_main_page(self,driver):
@@ -11,6 +12,7 @@ class TestLogo:
         main_page.wait_visibility_of_header_logo_scooter()
         main_page.click_on_header_logo_scooter()
         main_page.wait_visibility_of_main_header()
+        assert main_page.check_displaying_of_main_header()
 
     @allure.title('Проверка перехода на страницу "Дзена" при клике на лого "Яндекс"')
     def test_logo_dzen(self, driver):
@@ -19,4 +21,3 @@ class TestLogo:
         main_page.click_on_header_logo_yandex()
         main_page.switch_to_next_tab()
         assert 'Дзен' in main_page.get_page_title()
-
